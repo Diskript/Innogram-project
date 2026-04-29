c70cfb746714e3baf9c1ea6e30297efaf0f6ad75
Diskript <i22901ib@gmail.com>
2026-04-29 18:37:40 +0300

    i hate this shi will compolete fixing docker compose not building properly later

--- packages/database/prisma/prisma.config.ts
+++ packages/database/prisma/prisma.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env<Env>("DATABASE_URL"),
+    url: process.env.DATABASE_URL || env<Env>("DATABASE_URL"),
   },
 });
